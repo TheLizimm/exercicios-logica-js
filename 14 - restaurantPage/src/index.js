78478b048d77528ccdd9470bc7ac78c1d7fada4a
TheLizimm <luisfelipecdsb@gmail.com>
2025-11-21 13:18:16 -0300

    :lipstick: feat: alteração na pagina contato, nos botões e na imagem que segue quando vc clicar no botão

--- 14 - restaurantPage/src/index.js
+++ 14 - restaurantPage/src/index.js
@@ -6,6 +6,9 @@ if (imageElement) {
 }
 
 function hideInfos() {
+    const imgHome = document.getElementById('dripping-Homeimage')
+    const imgMenu = document.getElementById('dripping-Menuimage')
+    const imgContact = document.getElementById('dripping-Contactimage')
     const contentToHome = document.getElementById('storageHome');
     const contentToMenu = document.getElementById('storageMenu')
     const contentoToContact = document.getElementById('storageContact')
@@ -13,14 +16,19 @@ function hideInfos() {
     const content = document.getElementById('content')
     const homeButton = document.getElementById('coverHome');
     const menuButton = document.getElementById('coverMenu');
-    const contactButton = document.getElementById('coverContact');
+    const contactB = document.getElementById('coverContact');
+    const coverButtons = [homeButton, menuButton, contactB];
     if (homeButton) {
         homeButton.addEventListener('click', () => {
             contentToHome.style.display = 'block';
             contentToMenu.style.display = 'none';
             contentoToContact.style.display = 'none'
+            imgHome.style.visibility = 'visible'; 
+            imgContact.style.visibility = 'hidden'
+            imgMenu.style.visibility = 'hidden'
             container.style.height = '140vh'
             content.style.height = '125vh'
+            setButton(homeButton)
             console.log('Home Clicado. Conteúdo visível.');
         });
     }
@@ -29,19 +37,43 @@ function hideInfos() {
             contentToHome.style.display = 'none';
             contentToMenu.style.display = 'block';
             contentoToContact.style.display = 'none'
+            imgHome.style.visibility = 'hidden'; 
+            imgContact.style.visibility = 'hidden'
+            imgMenu.style.visibility = 'visible'
             container.style.height = '620vh'
             content.style.height = '610vh'
+            setButton(menuButton)
             console.log('Menu Clicado. Conteúdo oculto.');
         });
     }
-    if (contactButton) {
-        contactButton.addEventListener('click', () => {
+    if (contactB) {
+        contactB.addEventListener('click', () => {
             contentToHome.style.display = 'none';
             contentToMenu.style.display = 'none';
             contentoToContact.style.display = 'block'
+            imgHome.style.visibility = 'hidden'; 
+            imgContact.style.visibility = 'visible'
+            imgMenu.style.visibility = 'hidden'
+            container.style.height = '115vh'
+            content.style.height = '100vh'
+            setButton(contactB)
             console.log('Contact Clicado. Conteúdo oculto.');
         });
     }
+    function setButton(button){
+        coverButtons.forEach(btn =>{
+            if(btn){
+                btn.classList.remove('activeCover');
+            }
+        });
+        if(button){
+            button.classList.add('activeCover');
+        }
+    }
+    setButton(homeButton)
+    imgHome.style.visibility = 'visible'; 
+    imgMenu.style.visibility = 'hidden';
+    imgContact.style.visibility = 'hidden';
 }
 
 
